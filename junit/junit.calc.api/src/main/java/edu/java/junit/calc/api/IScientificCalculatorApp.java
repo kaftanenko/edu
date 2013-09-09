7011978c1a5e74430b723edd492eecc9ed62dbef
Artem Kaftanenko <java.developer@gmx.com>
2013-09-09 23:02:21 +0200

    [edu.java.junit] ... (1) existing demo tests restructred and renamed; (2) two more new tests implemented.
    	(1) following tests restructured and renamed due to improve tests
    	    expressiveness and to correspond to the presentation chapters
    	    numbering:
    
    		- Unit1_SimpleCalculator_Addition_SimpleTest.java
    		- Unit3_SimpleCalculator_Division_FurtherAnnotationsTest.java
    		- Unit4_SimpleCalculator_Multiplication_SpringDependencyInjectionTest.java
    
    	(2) following new tests implemented due to demonstrate some new
    	    JUnit aspects:
    
    		- Unit2_SimpleCalculator_Subtruction_ParameterizedTest.java
    		- Unit5_SimpleCalculator_MultiplicationAndDivision_TestSuite.java

---
+++ junit/junit.calc.api/src/main/java/edu/java/junit/calc/api/IScientificCalculatorApp.java
@@ -0,0 +1,13 @@
+package edu.java.junit.calc.api;
+
+public interface IScientificCalculatorApp extends ISimpleCalculatorApp {
+
+	// ... business methods
+
+	public long ln(long arg);
+
+	public long lg(long arg);
+
+	public long log(long arg1, long arg2);
+
+}
---
+++ junit/junit.calc.tests-solution/src/test/java/edu/java/junit/calc/Unit4_SimpleCalculator_Multiplication_SpringDependencyInjectionTest.java
@@ -0,0 +1,111 @@
+package edu.java.junit.calc;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import edu.java.junit.calc.api.ISimpleCalculatorApp;
+import edu.java.junit.calc.util.SpringTestHelper;
+
+/**
+ * <b>Unit 4</b>: ... demonstrates JUnit initialization using dependency
+ * injection engine (Spring).
+ * 
+ * @author java.developer
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({ SpringTestHelper.CONTEXT_LOCATION, SpringTestHelper.TEST_CONTEXT_LOCATION })
+public class Unit4_SimpleCalculator_Multiplication_SpringDependencyInjectionTest {
+
+	// ... properties
+
+	@Autowired
+	private ISimpleCalculatorApp calculatorUnderTest;
+
+	// ... test methods
+
+	@Test
+	public void test_Multiplication_Of_Two_Positive_Numbers() {
+
+		final long arg1 = 2;
+		final long arg2 = 3;
+
+		long result = calculatorUnderTest.mul(arg1, arg2);
+
+		long expectedResult = 6;
+		assertEquals(expectedResult, result);
+	}
+
+	@Test
+	public void test_Multiplication_Of_Two_Negative_Numbers() {
+
+		final long arg1 = -4;
+		final long arg2 = -5;
+
+		long result = calculatorUnderTest.mul(arg1, arg2);
+
+		long expectedResult = 20;
+		assertEquals(expectedResult, result);
+	}
+
+	@Test
+	public void test_Multiplication_Of_Negative_And_Positive_Numbers() {
+
+		final long arg1 = -6;
+		final long arg2 = 7;
+
+		long result = calculatorUnderTest.mul(arg1, arg2);
+
+		long expectedResult = -42;
+		assertEquals(expectedResult, result);
+	}
+
+	@Test
+	public void test_Multiplication_Of_Positive_And_Zero_Numbers() {
+
+		final long arg1 = 7;
+		final long arg2 = 0;
+
+		long result = calculatorUnderTest.mul(arg1, arg2);
+
+		long expectedResult = 0;
+		assertEquals(expectedResult, result);
+	}
+
+	@Test
+	public void test_Multiplication_Of_Negative_And_Zero_Numbers() {
+
+		final long arg1 = -7;
+		final long arg2 = 0;
+
+		long result = calculatorUnderTest.mul(arg1, arg2);
+
+		long expectedResult = 0;
+		assertEquals(expectedResult, result);
+	}
+
+	@Test(expected = ArithmeticException.class)
+	// ... due to some kind of number overflow.
+	public void test_Multiplication_Of_MaxSupportingLongValue_And_2_WithExpecting_ArithmeticException() {
+
+		final long arg1 = Long.MAX_VALUE;
+		final long arg2 = 2;
+
+		calculatorUnderTest.mul(arg1, arg2);
+	}
+
+	@Test(expected = ArithmeticException.class)
+	// ... due to some kind of number overflow.
+	public void test_Multiplication_Of_MinSupportingLongValue_And_2_WithExpecting_ArithmeticException() {
+
+		final long arg1 = Long.MIN_VALUE;
+		final long arg2 = 2;
+
+		calculatorUnderTest.mul(arg1, arg2);
+	}
+
+}
