323dfd05f0a70aaaadbef92f51d844b90547da19
Artem Kaftanenko <java.developer@gmx.com>
2015-03-22 22:07:19 +0100

    [edu.java.junit] ... skeleton test classes appended into the "tests-skeleton" module.

---
+++ junit/junit.calc.tests-skeleton/src/test/java/edu/java/junit/calc/Unit1_SimpleCalculator_Addition_SimpleTest.java
@@ -0,0 +1,20 @@
+package edu.java.junit.calc;
+
+import edu.java.junit.calc.api.ISimpleCalculatorApp;
+
+/**
+ * <b>Unit 1</b>: ... JUnit simple JUnit Test.
+ * 
+ * @author java.developer
+ */
+public class Unit1_SimpleCalculator_Addition_SimpleTest {
+
+	// ... properties
+
+	private ISimpleCalculatorApp calculatorUnderTest;
+
+	// ... setUp/tearDown
+
+	// ... test methods
+
+}
---
+++ junit/junit.calc.tests-skeleton/src/test/java/edu/java/junit/calc/Unit5_SimpleCalculator_MultiplicationAndDivision_TestSuite.java
@@ -0,0 +1,11 @@
+package edu.java.junit.calc;
+
+
+/**
+ * <b>Unit 5</b>: ... demonstrates JUnit tests suite.
+ * 
+ * @author java.developer
+ */
+public class Unit5_SimpleCalculator_MultiplicationAndDivision_TestSuite {
+
+}
