489526a14c5468b2d92ff6b00b679f93ce953de2
Artem Kaftanenko <java.developer@gmx.com>
2016-04-23 12:23:30 +0200

    [edu.java.fws.osgi.bndtools] ... typical "calculator" project structure created using the "Bndtools" plugin for "eclipse" IDE.

---
+++ fws/frameworks.osgi/bndtools/osgi_bndtools.calculator.access.http/src/main/java/osgi_bndtools/calculator/access/http/Example.java
@@ -0,0 +1,10 @@
+package osgi_bndtools.calculator.access.http;
+
+import org.osgi.service.component.annotations.*;
+
+@Component
+public class Example {
+
+	// TODO: class provided by template
+
+}
---
+++ fws/frameworks.osgi/bndtools/osgi_bndtools.calculator.access.shell/src/main/java/osgi_bndtools/calculator/access/shell/Example.java
@@ -0,0 +1,10 @@
+package osgi_bndtools.calculator.access.shell;
+
+import org.osgi.service.component.annotations.*;
+
+@Component
+public class Example {
+
+	// TODO: class provided by template
+
+}
