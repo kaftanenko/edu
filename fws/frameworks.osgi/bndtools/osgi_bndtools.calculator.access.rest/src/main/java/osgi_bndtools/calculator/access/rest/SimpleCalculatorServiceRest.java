928828b52c4b49f9c0ec543cd886d17865e160ac
Artem Kaftanenko <java.developer@gmx.com>
2016-04-27 20:39:36 +0200

    [edu.java.fws.osgi.bndtools] ... the "http"-sub-bundle renamed to "rest" and a little bit modified.

---
+++ fws/frameworks.osgi/bndtools/osgi_bndtools.calculator.access.rest/src/main/java/osgi_bndtools/calculator/access/rest/SimpleCalculatorServiceRest.java
@@ -0,0 +1,40 @@
+package osgi_bndtools.calculator.access.rest;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import aQute.bnd.annotation.component.Component;
+import aQute.bnd.annotation.component.Reference;
+import osgi_bndtools.calculator.service.api.ISimpleCalculatorService;
+
+@Path("rest")
+@Component(provide=Object.class)
+public class SimpleCalculatorServiceRest {
+	
+	// ... properties
+
+	private ISimpleCalculatorService calculationService;
+
+	// ... dependency injection methods
+
+	@Reference
+	public void setCalculationService(ISimpleCalculatorService calculationService) {
+
+		this.calculationService = calculationService;
+	}
+
+	// ... command handling methods
+
+
+	  @GET
+	  @Produces(MediaType.APPLICATION_JSON)
+	  public List<String> list() {
+		return Arrays.asList("A", "B", "C");
+	  }
+	  
+}
