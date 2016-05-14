2c8ef1fb2d39620a2cb024344f66f79327fe026a
Artem Kaftanenko <java.developer@gmx.com>
2016-05-14 08:40:17 +0200

    [edu.java.fws.osgi.bndtools] ... "web.jsf" the simple calculator GUI extracted from the main page into a reusable UI-composite named <widget:simple-calculator>.

--- fws/frameworks.osgi/bndtools/osgi_bndtools.calculator.access.ui.web.jsf/src/main/java/osgi_bndtools/calculator/access/web/jsf/SimpleCalculatorBean.java
+++ fws/frameworks.osgi/bndtools/osgi_bndtools.calculator.access.ui.web.jsf/src/main/java/osgi_bndtools/calculator/access/web/jsf/SimpleCalculatorBean.java
@@ -10,23 +10,6 @@ import osgi_bndtools.calculator.access.ui.common.type.EMathOperation;
 @SessionScoped
 public class SimpleCalculatorBean extends CalculatorViewController {
 	
-	// ... constants
-
-	public final static String PARAM_NAME_NUMBER_BUTTON = "num";
-	public final static String PARAM_NAME_COMMA_BUTTON = "comma";
-	public final static String PARAM_NAME_OPERATION_BUTTON = "op";
-
-	public final static String PARAM_VALUE_OPERATION_ADD = EMathOperation.ADD.name();
-	public final static String PARAM_VALUE_OPERATION_DIV = EMathOperation.DIVIDE.name();
-	public final static String PARAM_VALUE_OPERATION_MUL = EMathOperation.MULTIPLY.name();
-	public final static String PARAM_VALUE_OPERATION_NEG = EMathOperation.NEGATE.name();
-	public final static String PARAM_VALUE_OPERATION_SUB = EMathOperation.SUBTRACT.name();
-
-	public final static String PARAM_VALUE_OPERATION_CANCEL = "cancel";
-	public final static String PARAM_VALUE_OPERATION_DELETE = "delete";
-
-	public final static String PARAM_VALUE_OPERATION_CALCULATE = "calculate";
-	
 	// ... business methods
 	
 	public void setDisplayValue(final String displayValue) {}
