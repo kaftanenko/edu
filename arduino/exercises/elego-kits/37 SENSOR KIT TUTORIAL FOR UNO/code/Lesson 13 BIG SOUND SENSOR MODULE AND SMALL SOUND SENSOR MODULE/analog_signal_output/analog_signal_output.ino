afa912b70c4c059a598bbd32d2f490788f60261f
Artem Kaftanenko <java.developer@gmx.com>
2016-09-20 21:51:23 +0200

    [edu.arduino] ... new exercises for the 2.8inch-TFT kit from the "Elego" appended: "Arduino Demo_UNO&Mega2560".

--- arduino/exercises/elego-kits/37 SENSOR KIT TUTORIAL FOR UNO/code/Lesson 13 BIG SOUND SENSOR MODULE AND SMALL SOUND SENSOR MODULE/analog_signal_output/analog_signal_output.ino
+++ arduino/exercises/elego-kits/37 SENSOR KIT TUTORIAL FOR UNO/code/Lesson 13 BIG SOUND SENSOR MODULE AND SMALL SOUND SENSOR MODULE/analog_signal_output/analog_signal_output.ino
@@ -3,17 +3,21 @@ int  sensorPin  =  A5;     // select the input  pin for  the potentiometer
 int  ledPin  =  13;   // select the pin for  the LED
 int  sensorValue =  0;  // variable to  store  the value  coming  from  the sensor
 
-void setup()
-{
+void setup() {
   pinMode(ledPin, OUTPUT);
   Serial.begin(9600);
 }
+
 void loop() {
+  
   sensorValue =  analogRead(sensorPin);
-  digitalWrite(ledPin,  HIGH);
-  delay(sensorValue);
-  digitalWrite(ledPin,  LOW);
-  delay(sensorValue);
-  Serial.println(sensorValue,  DEC);
+  //digitalWrite(ledPin,  HIGH);
+  //delay(sensorValue);
+  //digitalWrite(ledPin,  LOW);
+  //delay(sensorValue);
+  
+  if (sensorValue > 500) {
+    Serial.println(sensorValue,  DEC);
+  }
 }
 
