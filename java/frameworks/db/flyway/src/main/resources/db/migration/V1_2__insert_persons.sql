370d1dfb0640c1807c71d4e6c76db8dcc565308c
Artem Kaftanenko <java.developer@gmx.com>
2017-03-19 15:43:19 +0100

    [edu.java.fws.flyway] ... FlyWay technology demonstrator project created. The project includes basic functionality to be used within Maven projects.

---
+++ java/frameworks/db/flyway/src/main/resources/db/migration/V1_2__insert_persons.sql
@@ -0,0 +1,3 @@
+insert into PERSON${schemaSuffix} (ID, NAME) values (1, 'Axel');
+insert into PERSON${schemaSuffix} (ID, NAME) values (2, 'Mr. Foo');
+insert into PERSON${schemaSuffix} (ID, NAME) values (3, 'Ms. Bar');
