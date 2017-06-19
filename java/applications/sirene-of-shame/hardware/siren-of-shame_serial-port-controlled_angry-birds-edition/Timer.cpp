6a7198af37ad5d1d4649a725cabd75e0320269ba
Artem Kaftanenko <java.developer@gmx.com>
2017-06-19 23:51:04 +0200

    [app.siren-of-shame.hardware] ... "Angry Birds" edition implemented.

---
+++ java/applications/sirene-of-shame/hardware/siren-of-shame_serial-port-controlled_angry-birds-edition/Timer.cpp
@@ -0,0 +1,13 @@
+#include "Arduino.h"
+#include "Timer.h"
+
+bool Timer::isOver(long timeoutInMs) {
+
+  long currentTime = millis();
+  return (currentTime - _timerStartTime) > timeoutInMs;
+}
+
+void Timer::reset() {
+
+  _timerStartTime = millis();
+}
