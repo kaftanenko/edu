ad6ec124e72f5e367d80795fb008979bcb812bcc
Artem Kaftanenko <java.developer@gmx.com>
2017-06-19 23:51:04 +0200

    [app.siren-of-shame.hardware] ... "Fire Trucks" edition aligned to the current "Angry Birds" implementations state:
     - full map of control pins states included into the each alarm level
       configuration due to avoid unnessesary short switching effects (so
       called "snow effect") and to offer extended control over possible
       control pins state pattterns.

--- java/applications/siren-of-shame/hardware/siren-of-shame_serial-port-controlled_fire-trucks-edition/siren-of-shame_serial-port-controlled_fire-trucks-edition.ino
+++ java/applications/siren-of-shame/hardware/siren-of-shame_serial-port-controlled_fire-trucks-edition/siren-of-shame_serial-port-controlled_fire-trucks-edition.ino
@@ -30,7 +30,7 @@ const String CONFIG_COMMANDS_MESSAGE = "I understand following commands: '"
                                        + "'.";
 
 const int CONFIG_SERIAL_PORT_BAUD_RATE__9600 = 9600;
-const int CONFIG_CHANNEL_LIGHTS_TAKT_DURATION_IN_MS__400 = 400;
+const int CONFIG_CHANNEL_LIGHTS_TAKT_DURATION_IN_MS__600 = 600;
 
 enum AlarmLevel {
 
@@ -40,22 +40,40 @@ enum AlarmLevel {
 
   RED_EXPECTING_UPDATE,
   YELLOW_EXPECTING_UPDATE,
-  GREENBLUE_EXPECTING_UPDATE
+  GREENBLUE_EXPECTING_UPDATE,
 };
 
 enum LightState {
 
   LIGHT_ON,
-  LIGHT_OFF
+  LIGHT_OFF,
 };
 
 enum SoundEffectsState {
 
   SOUND_EFFECTS_ON,
-  SOUND_EFFECTS_OFF
+  SOUND_EFFECTS_OFF,
 };
 
 // ... configuration
+
+class ControlPinState {
+
+  public:
+
+    int _number;
+    int _state;
+
+    ControlPinState(int pinNumber, int pinState);
+};
+
+ControlPinState::ControlPinState(int pinNumber, int pinState) {
+
+  _number = pinNumber;
+  _state = pinState;
+};
+
+// ...
 
 class AlarmLevelConfig {
 
@@ -64,36 +82,67 @@ class AlarmLevelConfig {
     AlarmLevel _alarmLevel;
     String _alarmLevelName;
 
-    int _controlPinLight1;
-    int _controlPinLight2;
+    ControlPinState *_controlPinsState;
 
     long _rememberingTaktInMs;
     SoundEffectsState _rememberingSoundEffectsState;
 
-    AlarmLevelConfig(AlarmLevel alarmLevel, String alarmLevelName, int controlPinLight1, int controlPinLight2, SoundEffectsState rememberingSoundEffectsState, long rememberingTaktInSec);
+    AlarmLevelConfig(AlarmLevel alarmLevel, String alarmLevelName, ControlPinState *controlPinsState, SoundEffectsState rememberingSoundEffectsState, long rememberingTaktInSec);
 };
 
-AlarmLevelConfig::AlarmLevelConfig(AlarmLevel alarmLevel, String alarmLevelName, int controlPinLight1, int controlPinLight2, SoundEffectsState rememberingSoundEffectsState, long rememberingTaktInSec) {
+AlarmLevelConfig::AlarmLevelConfig(AlarmLevel alarmLevel, String alarmLevelName, ControlPinState *controlPinsState, SoundEffectsState rememberingSoundEffectsState, long rememberingTaktInSec) {
 
   _alarmLevel = alarmLevel;
   _alarmLevelName = alarmLevelName;
 
-  _controlPinLight1 = controlPinLight1;
-  _controlPinLight2 = controlPinLight2;
+  _controlPinsState = controlPinsState;
 
   _rememberingTaktInMs = rememberingTaktInSec * 1000;
   _rememberingSoundEffectsState = rememberingSoundEffectsState;
 };
 
+const int CONTROL_PINS[] = { 2, 3, 4, 5, 6, 7 };
+const int CONTROL_PINS_COUNT = 6;
+
+const ControlPinState controlPinsState_For_Red[] = {
+
+  ControlPinState(2, LOW),
+  ControlPinState(3, LOW),
+  ControlPinState(4, LOW),
+  ControlPinState(5, LOW),
+  ControlPinState(6, HIGH),
+  ControlPinState(7, HIGH),
+};
+
+const ControlPinState controlPinsState_For_Yellow[] = {
+
+  ControlPinState(2, LOW),
+  ControlPinState(3, LOW),
+  ControlPinState(4, HIGH),
+  ControlPinState(5, HIGH),
+  ControlPinState(6, LOW),
+  ControlPinState(7, LOW),
+};
+
+const ControlPinState controlPinsState_For_GreenBlue[] = {
+
+  ControlPinState(2, HIGH),
+  ControlPinState(3, HIGH),
+  ControlPinState(4, LOW),
+  ControlPinState(5, LOW),
+  ControlPinState(6, LOW),
+  ControlPinState(7, LOW),
+};
+
 AlarmLevelConfig alarmLevelConfigs[] = {
 
-  AlarmLevelConfig(RED, "RED", 2, 3, SOUND_EFFECTS_ON, 90),
-  AlarmLevelConfig(YELLOW, "YELLOW", 4, 5, SOUND_EFFECTS_ON, 120),
-  AlarmLevelConfig(GREENBLUE, "GREENBLUE", 6, 7, SOUND_EFFECTS_ON, 180),
+  AlarmLevelConfig(RED, "RED", controlPinsState_For_Red, SOUND_EFFECTS_ON, 120),
+  AlarmLevelConfig(YELLOW, "YELLOW", controlPinsState_For_Yellow, SOUND_EFFECTS_ON, 120),
+  AlarmLevelConfig(GREENBLUE, "GREENBLUE", controlPinsState_For_GreenBlue, SOUND_EFFECTS_ON, 180),
 
-  AlarmLevelConfig(RED_EXPECTING_UPDATE, "RED_EXPECTING_UPDATE", 2, 3, SOUND_EFFECTS_OFF, 90),
-  AlarmLevelConfig(YELLOW_EXPECTING_UPDATE, "YELLOW_EXPECTING_UPDATE", 4, 5, SOUND_EFFECTS_OFF, 120),
-  AlarmLevelConfig(GREENBLUE_EXPECTING_UPDATE, "GREENBLUE_EXPECTING_UPDATE", 6, 7, SOUND_EFFECTS_ON, 180)
+  AlarmLevelConfig(RED_EXPECTING_UPDATE, "RED_EXPECTING_UPDATE", controlPinsState_For_Red, SOUND_EFFECTS_OFF, 120),
+  AlarmLevelConfig(YELLOW_EXPECTING_UPDATE, "YELLOW_EXPECTING_UPDATE", controlPinsState_For_Yellow, SOUND_EFFECTS_OFF, 120),
+  AlarmLevelConfig(GREENBLUE_EXPECTING_UPDATE, "GREENBLUE_EXPECTING_UPDATE", controlPinsState_For_GreenBlue, SOUND_EFFECTS_ON, 180),
 };
 
 // ... properties
@@ -121,15 +170,13 @@ void setup()
   println_SerialPort_Message(CONFIG_GREETING_MESSAGE);
   println_SerialPort_Message(CONFIG_COMMANDS_MESSAGE);
 
-  initAlarmLevel_ControlPinsSet(alarmLevelConfigs[RED]);
-  initAlarmLevel_ControlPinsSet(alarmLevelConfigs[YELLOW]);
-  initAlarmLevel_ControlPinsSet(alarmLevelConfigs[GREENBLUE]);
+  initAlarmLevel_ControlPinsSet();
 
   // ... init sound alarmLevel
 
   mySoftwareSerial.begin(9600);
 
-  println_SerialPort_Message("");
+  // println_SerialPort_Message("");
   // println_SerialPort_Message("DFPlayer initializing ... (May take 3~5 seconds)");
 
   if (!myDFPlayer.begin(mySoftwareSerial)) { // Use softwareSerial to communicate with mp3.
@@ -141,7 +188,7 @@ void setup()
   // println_SerialPort_Message("DFPlayer Mini is online.");
 
   myDFPlayer.volume(30);  // ... 0 to 30
-  playSoundEffect_Any_Within_Folder(7);
+  // playSoundEffect_Any_Within_Folder(7);
 }
 
 void loop()
@@ -158,31 +205,37 @@ void loop()
       println_SerialPort_Message(COMMAND_PING_RESPONSE_SUCCEEDED);
     } else if (lastCommand == COMMAND_SET_ALARM_LEVEL_TO_RED) {
       doSetState(RED);
-    } else if (lastCommand == COMMAND_SET_ALARM_LEVEL_TO_RED_EXPECTING_UPDATE) {
-      doSetState(RED_EXPECTING_UPDATE);
     } else if (lastCommand == COMMAND_SET_ALARM_LEVEL_TO_YELLOW) {
       doSetState(YELLOW);
-    } else if (lastCommand == COMMAND_SET_ALARM_LEVEL_TO_YELLOW_EXPECTING_UPDATE) {
-      doSetState(YELLOW_EXPECTING_UPDATE);
     } else if (lastCommand == COMMAND_SET_ALARM_LEVEL_TO_GREENBLUE) {
       doSetState(GREENBLUE);
+    } else if (lastCommand == COMMAND_SET_ALARM_LEVEL_TO_RED_EXPECTING_UPDATE) {
+      doSetState(RED_EXPECTING_UPDATE);
+    } else if (lastCommand == COMMAND_SET_ALARM_LEVEL_TO_YELLOW_EXPECTING_UPDATE) {
+      doSetState(YELLOW_EXPECTING_UPDATE);
     } else if (lastCommand == COMMAND_SET_ALARM_LEVEL_TO_GREENBLUE_EXPECTING_UPDATE) {
       doSetState(GREENBLUE_EXPECTING_UPDATE);
     } else {
-      println_SerialPort_Message("Command '" + lastCommand + "' is not supported.");
+      //println_SerialPort_Message("Command '" + lastCommand + "' is not supported.");
     }
   }
 
-  if (currentAlarmLevel_LightsState_Timer.isOver(CONFIG_CHANNEL_LIGHTS_TAKT_DURATION_IN_MS__400)) {
+  if (currentAlarmLevel_LightsState_Timer.isOver(CONFIG_CHANNEL_LIGHTS_TAKT_DURATION_IN_MS__600)) {
 
     currentAlarmLevel_LightsState_Timer.reset();
     currentAlarmLevel_LightsState = currentAlarmLevel_LightsState == LIGHT_ON ? LIGHT_OFF : LIGHT_ON;
 
-    switchAlarmLevel_Lights_Off(alarmLevelConfigs[RED]);
-    switchAlarmLevel_Lights_Off(alarmLevelConfigs[YELLOW]);
-    switchAlarmLevel_Lights_Off(alarmLevelConfigs[GREENBLUE]);
+    for (int i = 0; i < CONTROL_PINS_COUNT; i++) {
 
-    switchAlarmLevel_Lights_To(*currentAlarmLevelConfig, currentAlarmLevel_LightsState);
+      int controlPinNumber = currentAlarmLevelConfig->_controlPinsState[i]._number;
+      int controlPinState = currentAlarmLevelConfig->_controlPinsState[i]._state;
+
+      if (controlPinState == LOW) {
+        digitalWrite(controlPinNumber, LOW);
+      } else {
+        digitalWrite(controlPinNumber, currentAlarmLevel_LightsState == LIGHT_ON ? HIGH : LOW);
+      }
+    }
   }
 
   if (currentAlarmLevel_Remembering_Timer.isOver(currentAlarmLevelConfig->_rememberingTaktInMs)) {
@@ -206,12 +259,12 @@ void doSetState(AlarmLevel newAlarmLevel) {
 
     playSoundEffect_On_ChangeTo(currentAlarmLevelConfig->_alarmLevel, newAlarmLevel);
 
+    currentAlarmLevelConfig = &(alarmLevelConfigs[newAlarmLevel]);
+
     currentAlarmLevel_LightsState_Timer.reset();
-    currentAlarmLevel_LightsState = LIGHT_OFF;
+    currentAlarmLevel_LightsState = LIGHT_ON;
 
     currentAlarmLevel_Remembering_Timer.reset();
-
-    currentAlarmLevelConfig = &(alarmLevelConfigs[newAlarmLevel]);
 
     println_SerialPort_Message("AlarmLevel changed to: " + currentAlarmLevelConfig->_alarmLevelName);
   } else {
@@ -231,7 +284,7 @@ String read_SerialPort_Message() {
   String message = "";
   while (Serial.available() > 0) {
     message += char(Serial.read());
-    delay(2);
+    delay(10);
   }
   return message;
 }
@@ -249,21 +302,13 @@ void println_SerialPort_Message(String message) {
 
 // ...
 
-void initAlarmLevel_ControlPinsSet(AlarmLevelConfig alarmLevelConfig) {
+void initAlarmLevel_ControlPinsSet() {
 
-  pinMode(alarmLevelConfig._controlPinLight1, OUTPUT);
-  pinMode(alarmLevelConfig._controlPinLight2, OUTPUT);
-}
+  for (int i = 0; i < CONTROL_PINS_COUNT; i++) {
 
-void switchAlarmLevel_Lights_Off(AlarmLevelConfig alarmLevelConfig) {
-
-  switchAlarmLevel_Lights_To(alarmLevelConfig, LIGHT_OFF);
-}
-
-void switchAlarmLevel_Lights_To(AlarmLevelConfig alarmLevelConfig, LightState toLightState) {
-
-  digitalWrite(alarmLevelConfig._controlPinLight1, toLightState == LIGHT_ON ? HIGH : LOW);
-  digitalWrite(alarmLevelConfig._controlPinLight2, toLightState == LIGHT_ON ? HIGH : LOW);
+    int controlPinNumber = CONTROL_PINS[i];
+    pinMode(controlPinNumber, OUTPUT);
+  }
 }
 
 // ...
