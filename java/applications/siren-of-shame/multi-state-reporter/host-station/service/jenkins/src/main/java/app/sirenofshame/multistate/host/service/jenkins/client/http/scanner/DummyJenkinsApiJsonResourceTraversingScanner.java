9a9445df8687cf628d760939bda7e08fe8d9b420
Artem Kaftanenko <java.developer@gmx.com>
2018-03-19 04:08:53 +0100

    [app.siren-of-shame.host-controller] ... DummyJenkinsApiJsonResourceTraversingScanner: small changes.

--- java/applications/siren-of-shame/multi-state-reporter/host-station/service/jenkins/src/main/java/app/sirenofshame/multistate/host/service/jenkins/client/http/scanner/DummyJenkinsApiJsonResourceTraversingScanner.java
+++ java/applications/siren-of-shame/multi-state-reporter/host-station/service/jenkins/src/main/java/app/sirenofshame/multistate/host/service/jenkins/client/http/scanner/DummyJenkinsApiJsonResourceTraversingScanner.java
@@ -1,6 +1,7 @@
 package app.sirenofshame.multistate.host.service.jenkins.client.http.scanner;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 
 import org.apache.commons.io.FileUtils;
@@ -11,7 +12,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import app.sirenofshame.common.host.service.jenkins.client.http.scanner.JenkinsApiJsonResourceScanner;
 import app.sirenofshame.common.host.service.jenkins.client.http.scanner.JenkinsApiJsonResourceScannerConfig;
 
-public class DummyJenkinsApiJsonResourceTraversingScanner extends JenkinsApiJsonResourceScanner {
+public class DummyJenkinsApiJsonResourceTraversingScanner //
+    extends JenkinsApiJsonResourceScanner //
+{
 
   // ... constructors
 
@@ -24,16 +27,32 @@ public class DummyJenkinsApiJsonResourceTraversingScanner extends JenkinsApiJson
   protected Map<String, Object> callJsonApi(final String resourcePath) {
 
     try {
-      final File file = new File(this.getClass().getResource("jenkins-info-mock-data.json").toURI());
-      final String bodyAsString = FileUtils.readFileToString(file);
 
+      final String bodyAsString = getMockData();
       final Map<String, Object> jsonContentAsMap = //
           new ObjectMapper().readValue(bodyAsString, new TypeReference<Map<String, Object>>() {
           });
       return jsonContentAsMap;
     } catch (final Exception ex) {
+      LOG.error(ex);
       throw new RuntimeException(ex);
     }
+  }
+
+  // ... helper methods
+
+  private String bodyAsString = null;
+
+  private String getMockData() throws IOException {
+
+    if (bodyAsString == null) {
+
+      final File file = new File(
+          "D:/projects/it/edu/projects/java/applications/siren-of-shame/multi-state-reporter/host-station/service/jenkins/src/main/resources/jenkins-info-mock-data.json");
+      bodyAsString = FileUtils.readFileToString(file);
+    }
+
+    return bodyAsString;
   }
 
 }
