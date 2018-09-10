3370fa9ac8d666cdbbc5dec45c8d927e8bdfabca
Artem Kaftanenko <java.developer@gmx.com>
2018-09-11 00:58:15 +0200

    [edu.java.app.calculator] ... "access.ui.web.ng" project skeleton updated using @angualar/cli@6.2.1.

--- java/applications/calculator/access/access-ui-web-ng/e2e/src/app.po.ts
+++ java/applications/calculator/access/access-ui-web-ng/e2e/src/app.po.ts
@@ -1,6 +1,6 @@
-import { browser, element, by } from 'protractor';
+import { browser, by, element } from 'protractor';
 
-export class App.Calculator.Access.Ui.Web.NgPage {
+export class AppPage {
   navigateTo() {
     return browser.get('/');
   }
