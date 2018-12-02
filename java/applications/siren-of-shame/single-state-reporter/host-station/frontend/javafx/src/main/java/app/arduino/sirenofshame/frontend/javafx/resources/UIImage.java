0d4e07a220c40cc67d25770c2763ab8af5f2d3a5
Artem Kaftanenko <java.developer@gmx.com>
2018-12-03 00:14:32 +0100

    [sirenofshame/single-state-reporter] ... JavaFX client: toolbar icons exchanged.

--- java/applications/siren-of-shame/single-state-reporter/host-station/frontend/javafx/src/main/java/app/arduino/sirenofshame/frontend/javafx/resources/UIImage.java
+++ java/applications/siren-of-shame/single-state-reporter/host-station/frontend/javafx/src/main/java/app/arduino/sirenofshame/frontend/javafx/resources/UIImage.java
@@ -8,23 +8,20 @@ public enum UIImage {
 
   APPLICATION_LOGO_36("jenkins/head.png"),
 
-  BUTTON_CONNECT_24("server/connected-24.png"),
-  BUTTON_CONNECT_36("server/connected-36.png"),
-  BUTTON_CONNECT_48("server/connected-48.png"),
-  BUTTON_DISCONNECT_24("server/disconnected-24.png"),
-  BUTTON_DISCONNECT_36("server/disconnected-36.png"),
-  BUTTON_DISCONNECT_48("server/disconnected-48.png"),
+  BUTTON_CONNECT_32("server/switch-on-icon-32.png"), //
+  BUTTON_DISCONNECT_32("server/switch-off-icon-32.png"), //
+  BUTTON_CONFIGURATION_32("server/settings-icon_3-32.png"), //
 
-  JENKINS_STATE_RED_32("jenkins/state/red.png"),
-  JENKINS_STATE_RED_ANIME_32("jenkins/state/red.png"),
+  JENKINS_STATE_RED_32("jenkins/state/red.png"), //
+  JENKINS_STATE_RED_ANIME_32("jenkins/state/red.png"), //
 
-  JENKINS_STATE_YELLOW_32("jenkins/state/yellow.png"),
-  JENKINS_STATE_YELLOW_ANIME_32("jenkins/state/yellow.png"),
+  JENKINS_STATE_YELLOW_32("jenkins/state/yellow.png"), //
+  JENKINS_STATE_YELLOW_ANIME_32("jenkins/state/yellow.png"), //
 
-  JENKINS_STATE_BLUE_32("jenkins/state/blue.png"),
-  JENKINS_STATE_BLUE_ANIME_32("jenkins/state/blue.png"),
+  JENKINS_STATE_BLUE_32("jenkins/state/blue.png"), //
+  JENKINS_STATE_BLUE_ANIME_32("jenkins/state/blue.png"), //
 
-  JENKINS_STATE_DISABLED_32("jenkins/state/disabled.png"),
+  JENKINS_STATE_DISABLED_32("jenkins/state/disabled.png"), //
   //
   ;
 
